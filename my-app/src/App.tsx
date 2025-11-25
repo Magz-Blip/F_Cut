1f1bcaa16dca6dac2930ddc09974131ece891d97
Magz-Blip <magnuselvsveen123@gmail.com>
2025-11-25 19:49:57 +0000

    Remove Tailwind utility classes from App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => {
   const navigate = useNavigate();
 
   return(
-    <div className='min-h-screen text-gray-800'>
+    <div>
       {/* Navbar*/}
       <header>
       </header>
@@ -16,16 +16,16 @@ const App: React.FC = () => {
           <h1 id='title'>
             ALLERCAK
           </h1>
-          <div className='flex space-x-4'>
+          <div>
             <button id='b1' 
               onClick={() => navigate('/login')}
-              className='hover:bg-black hover:text-white'>
+              >
               Login
           
             </button>
             <button id='b2'
               onClick={() => navigate('/signup')}
-              className='hover:bg-[#0052CC] hover:shadow-lg'>
+              >
               Sign up 
             </button>
           </div>
